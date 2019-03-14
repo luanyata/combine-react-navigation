5227ce25d5e41ac59ba0e4fb93edd596fb9ffeb2
Luan Lima <luanyata@gmail.com>
2019-03-14 15:08:48 -0300

    Correção do nome das tabs; adição dos icones nas tabs

--- src/routers/tabNav.js
+++ src/routers/tabNav.js
@@ -1,25 +1,48 @@
 import React from 'react';
+import {Platform} from 'react-native'
 import {createBottomTabNavigator} from 'react-navigation'
 import SettingsStackNav from "./stackNavigations/settingsStackNav";
 import FeedStackNav from './stackNavigations/feedStackNav'
 import ProfileStackNav from './stackNavigations/profileStackNav'
+import {Ionicons} from '@expo/vector-icons'
 
 
 const DashboardTabNav = createBottomTabNavigator({
-    FeedStackNav,
-    ProfileStackNav,
-    SettingsStackNav
-}, {
-    navigationOptions: ({navigation}) => {
-
-        console.log({navigation}, 'nav');
-
-        const {routerName} = navigation.state.routes[navigation.state.index].routeName;
-        return {
-            header: null,
-            headerTitle: {routerName}
+        Feed: {
+            screen: FeedStackNav,
+            navigationOptions: {
+                tabBarIcon: ({tintColor}) => {
+                    return <Ionicons name={Platform.select({ios: 'ios-home', android: 'md-home'})} size={25}
+                                     color={tintColor}/>
+                }
+            }
+        },
+        Profile: {
+            screen: ProfileStackNav,
+            navigationOptions: {
+                tabBarIcon: ({tintColor}) => {
+                    return <Ionicons name={Platform.select({ios: 'ios-person', android: 'md-person'})} size={25}
+                                     color={tintColor}/>
+                }
+            }
+        },
+        Settings: {
+            screen: SettingsStackNav,
+            navigationOptions: {
+                tabBarIcon: ({tintColor}) => {
+                    return <Ionicons name={Platform.select({ios: 'ios-settings', android: 'md-settings'})} size={25}
+                                     color={tintColor}/>
+                }
+            }
         }
-    }
-});
+    },
+    {
+        navigationOptions: ({navigation}) => {
+            return {
+                header: null,
+            }
+        },
+    },
+);
 
 export default DashboardTabNav;
